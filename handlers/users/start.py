bd431ab61ce96adaadeb953c8c637dba706338d8
AzizbekReimov <120093969+AzizbekReimov@users.noreply.github.com>
2023-01-15 15:33:03 +0500

    Update start.py

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,9 +1,13 @@
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
+from keyboards.default.money_keyboard import smenani_ochish, smenani_yopish, pul_berish
+from data.config import ochet
+from datetime import datetime as dt
 
 from loader import dp
 
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f"Salom, {message.from_user.full_name}!")
+    ochet[dt.now().strftime("%D")]={}
+    await message.answer(f"Salom, {message.from_user.full_name}! {ochet}", reply_markup=smenani_ochish)
